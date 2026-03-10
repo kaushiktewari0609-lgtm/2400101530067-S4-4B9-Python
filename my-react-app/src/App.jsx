ab6e156455c922c6562de4e89e3baa82ff103616
kaushiktewari0609-lgtm <kaushiktewari0609@gmail.com>
2026-03-10 07:26:25 +0000

    exporting function

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Demo} from './component/Demo.jsx'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <User />
+    <div>
+      <Demo></Demo>
       <h1>Kaushik</h1>
-    </>
+    </div>
+      
+    
   );
 }
 
-export default App;
 
-function User() {
-  return (
-    <h1>My name is</h1>
-  );
-}
+
+
+export default Demo;
---
+++ my-react-app/src/component/Demo.jsx
@@ -0,0 +1,6 @@
+export function Demo()
+{
+    return(
+        <h1>This is example of demo</h1>
+    )
+}
