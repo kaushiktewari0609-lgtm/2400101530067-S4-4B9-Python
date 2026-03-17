a89327cb56b7209cdc235ee9992e0f73eec263b3
kaushiktewari0609-lgtm <kaushiktewari0609@gmail.com>
2026-03-17 07:39:50 +0000

    testing

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,21 +2,49 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Demo} from './component/Demo.jsx'
+
 
 function App() {
+
+  function name() {
+    return "kt";
+  }
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  function calc(a, b, op) {
+    if (op == '+') {
+      return "Sum is " + (a + b);
+    } 
+    else if (op == '-') {
+      return "Diff is " + (a - b);
+    } 
+    
+    
+    else {
+      return "Product is"+(a*b);
+    }
+  }
+  function table(a)
+  {
+    return 
+  }
+
   return (
     <div>
-      <Demo></Demo>
-      <h1>Kaushik</h1>
+      <h2>Introduction</h2>
+      <h1>------------</h1>
+      <h1>My name is {name()}</h1>
+      <h1>My age is {sum(10, 10)}</h1>
+      <h1>My maths is {calc(10, 10, '+')}</h1>
     </div>
-      
-    
   );
 }
 
+export default App;
 
 
 
 
-export default Demo;
