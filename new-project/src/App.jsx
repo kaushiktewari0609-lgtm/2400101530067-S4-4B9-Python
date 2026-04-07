b18607c3ca2261516998341d8ded22aeaba507d1
kaushiktewari0609-lgtm <kaushiktewari0609@gmail.com>
2026-04-07 07:28:38 +0000

    arrow function with and without parameters plus state in react

--- new-project/src/App.jsx
+++ new-project/src/App.jsx
@@ -3,10 +3,42 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  return (
-    <h1>welcome to new project</h1>
+function App() 
+{
+  let data="demo"
+  function update()
+  {
+    data="Example"
+    alert(data);
+  }
+  return(
+    <>
+    <h1>{data}</h1>
+    <button onClick={update}>testing</button>
+    </>
   )
+/*function abc()
+{
+  alert("function called")
 }
+  const fruit=()=>
+  {
+    alert("arrow function")
+  }
+  const apple=(name)=>
+  {
+    alert(name)
+  }
+  return (
+    <>
+    <h1>example of arrow function</h1>
+    <button onClick={()=>fruit()}>arrow function</button><br/>
+    <button onClick={abc}>click</button><br/>
+    <button onClick={()=>apple("click on apple")}>apple</button><br/>
+    <button onClick={()=>apple("click on banana")}>banana</button><br/>
+    </>
+  )*/
+}
+
 
 export default App
